4cfc9cb5a3915d0e698e6a3ff7e98891e2775d5a
kanika-i20 <kanika1389.becse24@chitkara.edu.in>
2025-02-08 16:22:29 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 int main(){
     char ch;
-    scanf("%c",ch);
+    scanf("%c",&ch);
     if(isalpha(ch))
     {
         else if(ch==a || ch==e || ch==i || ch==o || ch==u || ch==A || ch==E || ch==I || ch==O ch==U){
