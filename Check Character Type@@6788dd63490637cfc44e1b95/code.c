ace967ddc24c0da5420aa949d97e8d04609a33b4
kanika-i20 <kanika1389.becse24@chitkara.edu.in>
2025-02-11 18:11:29 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%c",&ch);
     if(isalpha(ch))
     {
-        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' ch=='U'){
+        else if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' ch=='U'){
         printf("Vowel");
         }
         else{
